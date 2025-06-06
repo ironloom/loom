9cd3e3b64af5ed9987a600eab5e51565bb093266
zewenn <takaczsoli@gmail.com>
2025-06-06 19:47:07 +0200

    add(repo): new example system

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const loom_mod = b.createModule(.{
-        .root_source_file = b.path("src/lib/root.zig"),
+        .root_source_file = b.path("lib/root.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -34,13 +34,6 @@ pub fn build(b: *std.Build) !void {
 
     loom_mod.addImport("uuid", uuid);
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/demo/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe_mod.addImport("loom", loom_mod);
-
     try b.modules.put("loom", loom_mod);
 
     const lib = b.addLibrary(.{
@@ -50,28 +43,34 @@ pub fn build(b: *std.Build) !void {
     });
     b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "loom-demo",
-        .root_module = exe_mod,
-    });
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const lib_unit_tests = b.addTest(.{ .root_module = loom_mod });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{ .root_module = exe_mod });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+
+    const examples: []const []const u8 = &.{
+        "spawning-removing",
+    };
+
+    inline for (examples) |example| {
+        const exe_mod = b.createModule(.{
+            .root_source_file = b.path("examples/" ++ example ++ "/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        exe_mod.addImport("loom", loom_mod);
+
+        const exe = b.addExecutable(.{
+            .name = example,
+            .root_module = exe_mod,
+        });
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        const run_step = b.step("example=" ++ example, "");
+        run_step.dependOn(&run_cmd.step);
+    }
 }

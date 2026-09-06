69bf65e39dff25bfb0b2ecf4b01becf08d7e1ebb
zewenn <takaczsoli@gmail.com>
2026-09-06 19:23:23 +0200

    add: global behaviour getter

--- lib/eventloop/Scene.zig
+++ lib/eventloop/Scene.zig
@@ -468,3 +468,23 @@ pub fn removeCameraById(self: *Self, id: []const u8) void {
 pub fn removeCameraByUuid(self: *Self, uuid_: u128) void {
     self.removeCamera(uuid_, uuidEqls(Camera));
 }
+
+pub fn getGlobalBehaviour(self: *Self, T: type) ?*T {
+    for (self.behaviours.items()) |*behaviour| {
+        if (behaviour.isType(T)) return behaviour.castBack(T);
+    }
+}
+pub inline fn pullGlobalBehaviour(self: *Self, T: type) !*T {
+    return self.getGlobalBehaviour(T) orelse error.GlobalBehaviourNotFound;
+}
+
+pub fn pullGlobalBehaviours(self: *Self, T: type) !lm.Array(*T) {
+    var list: lm.List(*T) = .init(lm.allocators.generic());
+    defer list.deinit();
+
+    for (self.behaviours.items()) |*behaviour| {
+        if (behaviour.isType(T)) try list.append(behaviour.castBack(T) orelse continue);
+    }
+
+    return try list.toArray();
+}
